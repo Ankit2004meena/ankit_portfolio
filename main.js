23cfb7dff77b3bce2e5bba647b87849344a758bc
Ankit2004meena <126344925+Ankit2004meena@users.noreply.github.com>
2023-12-31 13:06:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// import Typed from 'typed.js';
+// using https://github.com/mattboldt/typed.js
+var typed=new Typed(".text", {
+    strings:["Frontend Developer","YouTuber","Web developer","Electronics and Communication Engineer"],
+    typeSpeed: 90,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
